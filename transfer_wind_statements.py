9fb5900955eda754a123bebc96245af5b53acbfd
felixmon <37103200+felixmon@users.noreply.github.com>
2020-03-23 18:18:45 +0800

    Update transfer_wind_statements.py

--- transfer_wind_statements.py
+++ transfer_wind_statements.py
@@ -48,40 +48,49 @@ def data_cleaning(df):
     return df
 
 def main(argv=None):
+
+    # STAGE 1: OPEN
     input_filename = 'GSD.利润表[2488.HK].xlsx'
-    standard = 'STANDARD.xlsx'
     df = pd.read_excel(input_filename,engine='xlrd')
-    df_s = pd.read_excel(standard,engine='xlrd')
-    df_2ndH = df_s.iloc[193:317]
-    df_bi = df_s.iloc[0:193]
+
+    # STAGE 2: DATA CLEANING
+    # 思路：按行取数，得到2个serie, 一个是金额，另外一个时间节点（以index形式存在）
+    # 取完之后重新建立一个 dataframe，以上述2个series为2列，另外添加 headers，然后plot
+
+    # 读取Excel
     df = pd.DataFrame(df)
+    # 清洗数据（去掉无用行、空格，科目名称互换等）
     df = data_cleaning(df)
-    #df = df.reset_index()
-    col = '科目名称'
-    query_t = '主营业务收入'
-
-    # use is in to find row vaules
-    # if empty dataframe then len == 0.
-    #result = df[df.科目名称.isin([query_t])]
-    #result.reset_index(drop=True)
-    #print(result)
-    # result.iloc[0] with single label returns a series of values
-
+    # output clean dataset to excel for future usages
     df.to_excel('ref.xlsx',index=False)
 
+    # STAGE 3: SELECT, (CALCULATE), AND PLOT
+    # select single row, i.e. 净利润
+    # 对照清洗后的数据取对应的行
+    row1=21
+    # print the 科目名称 which is going to be used
+    row_name = df['科目名称'].iloc[row1]
+    print(row_name)
+    # drop column that will not be used
     df = df.drop(["科目名称"],axis=1)
-    df.T
     # sort columns, from old years to latest
+    # 按照阅读习惯，从左至右，从久到新排列时间
     df = df.reindex(sorted(df.columns), axis=1)
-    query_df=df.iloc[23]
+    # select single row i.e. 净利润
+    # iloc return a serie which contain only single row of values
+    query_df=df.iloc[row1]
+    # use serie.index as years
     year = list(query_df.index)
+    # convert to list in order to create dataframe
     value = list(query_df)
-
+    # create dataframe with 2 series/lists, using zip to combine the 2 lists.
     tdf = pd.DataFrame(list(zip(year,value)),columns=['year','value'])
 
     # select rows that are years with regex pattern
     # ref:https://stackoverflow.com/questions/15325182/how-to-filter-rows-in-pandas-by-regex
     tdf2 = tdf[tdf.year.str.contains(r'201.-12-31')]
+
+    print(tdf2)
 
     # 线性图: tdf2.plot(x='year')
     # 直方图：https://blog.csdn.net/Darkman_EX/article/details/80738021
